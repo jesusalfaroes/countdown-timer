c3f68f72e7bbd62bb2bc55c14d067635c981491f
Jesus Alfaro <39098569+jesusalfaroes@users.noreply.github.com>
2018-05-09 01:21:48 -0500

    Add files via upload

---
+++ Countdown Timer/js/main.js
@@ -0,0 +1,23 @@
+var futureDate = new Date("2019","01","01").getTime()+10000;
+var countdown = setInterval(function(){
+    var today = new Date().getTime();
+    var timeLeft = futureDate-today;
+    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+    console.log("Countdown = "+countdown);
+    if(timeLeft<0){
+        clearInterval(countdown);
+        alert("Countdown finished");
+        document.getElementById("days").innerHTML = 0;
+        document.getElementById("hours").innerHTML = 0;
+        document.getElementById("minutes").innerHTML = 0;
+        document.getElementById("seconds").innerHTML = 0;
+    }
+},1000);
+
